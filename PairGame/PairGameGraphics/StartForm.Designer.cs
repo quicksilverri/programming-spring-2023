0dec7ed66d75f7eae320e4a543fbc74e73ac799f
quicksilverri <eeyakhina@edu.hse.ru>
2023-06-21 21:16:43 +0500

    uhhh???

---
+++ PairGame/PairGameGraphics/StartForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PairGameGraphics
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.totalTime = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(12, 545);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(159, 54);
+            this.btnRestart.TabIndex = 0;
+            this.btnRestart.Text = "Новая игра";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.CreateNewGame);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(293, 545);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(159, 54);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.ExitGame);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.TimerTick);
+            // 
+            // totalTime
+            // 
+            this.totalTime.Interval = 1000;
+            this.totalTime.Tick += new System.EventHandler(this.GetTotalTime);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 611);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRestart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StartForm";
+            this.Text = "Парочки";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer totalTime;
+    }
+}
+
